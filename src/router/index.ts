cae8f7f96e77dad0e941828241c5043df246069b
mimi6612 <mimimimi6612@gmail.com>
2020-11-11 21:53:39 +0900

    perceptronのページ仮実装

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Perceptron from "../views/Perceptron.vue";
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/perceptron",
+    name: "Perceptron",
+    component: Perceptron
   }
 ];
 
---
+++ src/views/Perceptron.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="perceptron">
+    perceptron yeah!
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Perceptron",
+  components: {}
+});
+</script>
