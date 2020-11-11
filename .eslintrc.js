cd8fc24b7d0dd3349963937d2c7831b0d304d618
mimi6612 <mimimimi6612@gmail.com>
2020-11-11 21:46:43 +0900

    eslint対応

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,15 @@ module.exports = {
   env: {
     node: true
   },
-
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-
   parserOptions: {
     parser: "@typescript-eslint/parser"
   },
-
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
-
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/prettier",
     "@vue/typescript"
