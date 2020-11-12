c25362621f75847a20f0989e6282e7317c0fd29b
mimi6612 <mimimimi6612@gmail.com>
2020-11-12 16:39:56 +0900

    skelton.cssを導入

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 
+import "@/assets/Skeleton/css/normalize.css";
+import "@/assets/Skeleton/css/skeleton.css";
+
 createApp(App)
   .use(router)
   .use(store)
