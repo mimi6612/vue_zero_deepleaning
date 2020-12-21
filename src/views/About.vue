54b95308e07d68f38388c6728ccb246dbfc180f7
mimi6612 <mimimimi6612@gmail.com>
2020-12-21 17:39:29 +0900

    milligram導入

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>The base type is 1.6rem (16px) over 1.6 line height (24px)</p>
+
+    <!-- Other elements to text markup -->
+    <a>Anchor</a>
+    <em>Emphasis</em>
+    <small>Small</small>
+    <strong>Strong</strong>
+    <u>Underline</u>
+
+    <!-- Default Headings -->
+    <h1>Heading</h1>
+    <h2>Heading</h2>
+    <h3>Heading</h3>
+    <h4>Heading</h4>
+    <h5>Heading</h5>
+    <h6>Heading</h6>
+    <!-- Default Button -->
+    <a class="button" href="#">Default Button</a>
+
+    <!-- Outlined Button -->
+    <button class="button button-outline">
+      Outlined Button
+    </button>
+
+    <!-- Clear Button -->
+    <input class="button button-clear" type="submit" value="Clear Button" />
   </div>
 </template>
